481de4351860b1345eddbe9b49fb0e6519889f02
Mixarder <tarbitme@gmail.com>
2022-12-22 01:35:26 +0300

    Add ReadStr, create null README

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿Console.Write("Введите символ: ");
-string number = Console.ReadLine();
+﻿string number = ReadStr("Введите символ: ");
 int length = number.Length;
+
+string[] baseArray = GetArrayFromString(number);
 
 if (length <= 3)
     {
@@ -11,3 +12,23 @@ if (length <= 3)
     {
         Console.WriteLine();
     }
+
+
+string[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string[] res = new string[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = Convert.ToString(nums[i]);
+    }
+         
+    return res;
+}
+
+string ReadStr(string message)
+{
+    Console.Write(message);
+    return Convert.ToString(Console.ReadLine());
+}

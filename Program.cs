9ee57a397b74a3e4bf5ac5662975ac04592d87f3
Mixarder <tarbitme@gmail.com>
2022-12-22 19:29:43 +0300

    Yff vrode vse work-work))

--- Program.cs
+++ Program.cs
@@ -1,22 +1,34 @@
-﻿string number = ReadStr("Введите символ: ");
+﻿string number = ReadStr("Введите символы через запятую: ");
 int length = number.Length;
-
+string[] a = new string[0];
 string[] baseArray = GetArrayFromString(number);
+ 
+ void methodArray (string [] array){
+     string[] newarr = new string[array.Length];
+     for (int i=0; i<array.Length; i++){      
+        if(array[i].Length<=3){
+        newarr[i]=array[i];;
+        Console.WriteLine(newarr[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
 
-if (length <= 3)
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
     {
-    
-        Console.Write($" --> {number} ");
+        Console.Write($"{array[i]} ");
     }
-    else
-    {
-        Console.WriteLine();
-    }
+    Console.WriteLine();
+}
+
 
 
 string[] GetArrayFromString(string stringArray)
 {
-    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string[] nums = stringArray.Split(",", StringSplitOptions.RemoveEmptyEntries);
     string[] res = new string[nums.Length];
 
     for (int i = 0; i < nums.Length; i++)
@@ -32,3 +44,6 @@ string ReadStr(string message)
     Console.Write(message);
     return Convert.ToString(Console.ReadLine());
 }
+
+methodArray(baseArray);
+PrintArray(a);
